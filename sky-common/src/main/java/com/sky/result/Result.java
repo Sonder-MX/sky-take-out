a795dcdc86f1206b4fa1a069bb706af4f067cd9a
SonderMX <sonder-mx@outlook.com>
2023-07-13 23:07:14 +0800

    优化sky-common

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -6,14 +6,15 @@ import java.io.Serializable;
 
 /**
  * 后端统一返回结果
+ * 
  * @param <T>
  */
 @Data
 public class Result<T> implements Serializable {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
-    private String msg; //错误信息
-    private T data; //数据
+    private Integer code; // 编码：1成功，0和其它数字为失败
+    private String msg; // 错误信息
+    private T data; // 数据
 
     public static <T> Result<T> success() {
         Result<T> result = new Result<T>();
@@ -29,7 +30,7 @@ public class Result<T> implements Serializable {
     }
 
     public static <T> Result<T> error(String msg) {
-        Result result = new Result();
+        Result<T> result = new Result<>();
         result.msg = msg;
         result.code = 0;
         return result;
