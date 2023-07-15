4c73f3340cc3dc2befec620cda023c7d7ab63c39
SonderMX <sonder-mx@outlook.com>
2023-07-15 21:18:33 +0800

    swagger配置类 待完善

---
+++ sky-server/src/main/java/com/sky/config/SwaggerConfig.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 Zhejiang xiaominfo Technology CO.,LTD.
+ * All rights reserved.
+ * Official Web Site: http://www.xiaominfo.com.
+ * Developer Web Site: http://open.xiaominfo.com.
+ */
+
+package com.sky.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/***
+ * 创建Swagger配置
+ */
+@Configuration
+public class SwaggerConfig {
+    // TODO 待完善，不能正常使用
+    /**
+     * 根据@Tag 上的排序，写入x-order
+     *
+     * @return the global open api customizer
+     */
+    @Bean
+    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
+        return openApi -> {
+            if (openApi.getTags() != null) {
+                openApi.getTags().forEach(tag -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("x-order", tag.getName().length());
+                    tag.setExtensions(map);
+                });
+            }
+            if (openApi.getPaths() != null) {
+                openApi.addExtension("x-test123", "333");
+                openApi.getPaths().addExtension("x-abb", "333");
+            }
+
+        };
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("XXX用户系统API")
+                        .version("1.0")
+
+                        .description("Knife4j集成springdoc-openapi示例")
+                        .termsOfService("http://doc.xiaominfo.com")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://doc.xiaominfo.com")));
+    }
+
+}
