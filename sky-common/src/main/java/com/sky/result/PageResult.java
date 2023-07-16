d4ae2efebe1f55ad42fd170900ad1476919a5739
SonderMX <sonder-mx@outlook.com>
2023-07-16 22:54:14 +0800

    优化代码

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -13,10 +13,10 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PageResult implements Serializable {
+public class PageResult<T> implements Serializable {
 
     private long total; // 总记录数
 
-    private List<?> records; // 当前页数据集合
+    private List<T> records; // 当前页数据集合
 
 }
