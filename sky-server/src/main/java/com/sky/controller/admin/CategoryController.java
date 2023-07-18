eaa8cb1fbeb05dbfc0be7c614d32c1b7d3e900d2
SonderMX <sonder-mx@outlook.com>
2023-07-18 22:19:07 +0800

    分类controller

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,100 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+/**
+ * 分类管理
+ */
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * 
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    public Result<String> save(@RequestBody CategoryDTO categoryDTO) {
+        log.info("新增分类：{}", categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 分类分页查询
+     * 
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<PageResult<Category>> page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        log.info("分页查询：{}", categoryPageQueryDTO);
+        PageResult<Category> pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 删除分类
+     * 
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public Result<String> deleteById(Long id) {
+        log.info("删除分类：{}", id);
+        categoryService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * 修改分类
+     * 
+     * @param categoryDTO
+     * @return
+     */
+    @PutMapping
+    public Result<String> update(@RequestBody CategoryDTO categoryDTO) {
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 启用、禁用分类
+     * 
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    public Result<String> startOrStop(@PathVariable("status") Integer status, Long id) {
+        categoryService.startOrStop(status, id);
+        return Result.success();
+    }
+
+    /**
+     * 根据类型查询分类
+     * 
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List<Category>> list(Integer type) {
+        List<Category> list = categoryService.list(type);
+        return Result.success(list);
+    }
+}
