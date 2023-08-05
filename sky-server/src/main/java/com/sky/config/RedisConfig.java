311124b5cd847b569a095de8c568b3d08cfaac67
SonderMX <sonder-mx@outlook.com>
2023-08-05 17:05:04 +0800

    更新RedisConfig

--- sky-server/src/main/java/com/sky/config/RedisConfig.java
+++ sky-server/src/main/java/com/sky/config/RedisConfig.java
@@ -11,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @Slf4j
 public class RedisConfig {
-    @Bean // 作用：将方法的返回值放入到容器中，容器中这个组件默认的id就是方法名
-    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始配置redisTemplate");
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         // 设置连接工厂
@@ -20,7 +20,9 @@ public class RedisConfig {
         // 设置key的序列化器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         // 设置value的序列化器
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        // redisTemplate.setValueSerializer(new StringRedisSerializer());
+        // 使配置生效
+        redisTemplate.afterPropertiesSet();
         log.info("redisTemplate配置完成");
         return redisTemplate;
     }
