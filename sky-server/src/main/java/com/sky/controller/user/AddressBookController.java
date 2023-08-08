45323b2a7035a5fd286dbaff8d9c75fe27c11a07
SonderMX <sonder-mx@outlook.com>
2023-08-08 08:02:26 +0800

    地址簿功能开发

---
+++ sky-server/src/main/java/com/sky/controller/user/AddressBookController.java
@@ -0,0 +1,103 @@
+package com.sky.controller.user;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.AddressBook;
+import com.sky.result.Result;
+import com.sky.service.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/addressBook")
+public class AddressBookController {
+
+    @Autowired
+    private AddressBookService addressBookService;
+
+    /**
+     * 查询当前登录用户的所有地址信息
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List<AddressBook>> list() {
+        AddressBook addressBook = new AddressBook();
+        addressBook.setUserId(BaseContext.getCurrentId());
+        List<AddressBook> list = addressBookService.list(addressBook);
+        return Result.success(list);
+    }
+
+    /**
+     * 新增地址
+     *
+     * @param addressBook
+     * @return
+     */
+    @PostMapping
+    public Result<String> save(@RequestBody AddressBook addressBook) {
+        addressBookService.save(addressBook);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    public Result<AddressBook> getById(@PathVariable Long id) {
+        AddressBook addressBook = addressBookService.getById(id);
+        return Result.success(addressBook);
+    }
+
+    /**
+     * 根据id修改地址
+     *
+     * @param addressBook
+     * @return
+     */
+    @PutMapping
+    public Result<String> update(@RequestBody AddressBook addressBook) {
+        addressBookService.update(addressBook);
+        return Result.success();
+    }
+
+    /**
+     * 设置默认地址
+     *
+     * @param addressBook
+     * @return
+     */
+    @PutMapping("/default")
+    public Result<String> setDefault(@RequestBody AddressBook addressBook) {
+        addressBookService.setDefault(addressBook);
+        return Result.success();
+    }
+
+    /**
+     * 根据id删除地址
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public Result<String> deleteById(Long id) {
+        addressBookService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * 查询默认地址
+     */
+    @GetMapping("default")
+    public Result<AddressBook> getDefault() {
+        // SQL:select * from address_book where user_id = ? and is_default = 1
+        AddressBook addressBook = new AddressBook();
+        addressBook.setIsDefault(1);
+        addressBook.setUserId(BaseContext.getCurrentId());
+        List<AddressBook> list = addressBookService.list(addressBook);
+
+        if (list != null && list.size() == 1) {
+            return Result.success(list.get(0));
+        }
+
+        return Result.error("没有查询到默认地址");
+    }
+
+}
